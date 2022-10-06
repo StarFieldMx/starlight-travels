387470c8b0f4a939e4e5033c392c549c91bd417a
sebaszap123-dev <freeway.dev.sf@gmail.com>
2022-10-05 21:51:12 -0500

    fix parstime

--- lib/utils/parse_time.dart
+++ lib/utils/parse_time.dart
@@ -1,11 +1,14 @@
 String parseTime(int value) {
   String time = "";
-  if (value >= 60) {
-    final hours = value / 60;
-    double minutes = hours - hours.round();
-    minutes * 60;
-    minutes.floor();
+  if (value > 60) {
+    double hours = value / 60;
+    double minutes = hours - hours.toInt();
+    hours = hours - minutes;
+    minutes = minutes.abs() * 60;
+    minutes = minutes.floorToDouble();
     time = "${hours}h ${minutes.toString()}m";
+  } else if (value == 60) {
+    time = "1hr";
   } else {
     time = value.toString();
   }
--- lib/views/trips/user/my_trips_user.dart
+++ lib/views/trips/user/my_trips_user.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:provider/provider.dart';
 import 'package:starlight/models/flights.dart';
 import 'package:starlight/services/flights_services.dart';
-import 'package:starlight/views/payment/payment_screen.dart';
 import 'package:starlight/views/trips/widgets/my_flights_card.dart';
-import 'package:starlight/widgets/buttons/primary_button.dart';
+import 'package:starlight/widgets/loading_starlight.dart';
 
 class MyTripsViewUser extends StatefulWidget {
   const MyTripsViewUser({super.key});
@@ -24,20 +22,20 @@ class _MyTripsViewUserState extends State<MyTripsViewUser> {
   Widget build(BuildContext context) {
     final provider = Provider.of<FlightsServices>(context);
     if (provider.flights.isNotEmpty) {
-      Flights flight = provider.flights[0];
       return SafeArea(
-        child: Column(
-          children: [
-            MyFlightsCard(
-              flight: flight,
-            ),
-            MyFlightsCard(
-              flight: flight,
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: _parseChildren(provider.flights),
+          ),
         ),
       );
     }
-    return Center();
+    return const LoadinStarlight();
   }
+
+  List<Widget> _parseChildren(List<Flights> flights) => flights
+      .map((f) => MyFlightsCard(
+            flight: f,
+          ))
+      .toList();
 }
